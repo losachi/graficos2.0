b3b474992e80efbdf3535e7d402218b7ee246fb5
losachi <108551432+losachi@users.noreply.github.com>
2024-11-14 15:26:25 -0300

    dfg

--- graficos/redesSociaisMinhaEscola.js
+++ graficos/redesSociaisMinhaEscola.js
@@ -20,7 +20,7 @@ function processarDados(dados) {
         acc[redesSociais] = (acc[redesSociais] || 0) + 1
         return acc
     }, {})
-    const valores = ['47,9%','26,1%','17,8%','5,7%','2,5%']
+    const valores = ['47,9','26,1','17,8','5,7','2,5']
     const labels = ['Nordeste','Norte','Sudeste','Centro-Oeste','Sul']
 
     const data = [
