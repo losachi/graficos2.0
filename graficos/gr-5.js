3b44aeeb55f0fa090fc569570fa716af26a2c95b
losachi <108551432+losachi@users.noreply.github.com>
2024-11-21 15:25:27 -0300

    frerer

---
+++ graficos/gr-5.js
@@ -0,0 +1,57 @@
+import { getCSS, tickConfig, criarGrafico } from "./common.js"
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = ['Até 1908','Mais de 1908 a 2862','Mais de 2862 a 5724','Mais de 5724 a 9540', 'Mais de 9540 a 14310','Mais de 14310 a 23850','Mais de 23850']
+    const quantidadeDeUsuarios = ['17000','13000','20940','10000','4800','3400','1900',]
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Distribuição das famílias brasileiras por renda',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Renda mensal familiar em R$',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'número de famílias',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        }
+
+    }
+
+    criarGrafico(data, layout)
+}
+
+quantidadeUsuariosPorRede()
