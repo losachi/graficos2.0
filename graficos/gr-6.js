25b47a96181b798f904fa2f9f0596fd7481213b2
losachi <108551432+losachi@users.noreply.github.com>
2024-12-05 13:38:23 -0300

    dvgsf bc

--- graficos/gr-6.js
+++ graficos/gr-6.js
@@ -4,8 +4,8 @@ async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = ['Até 1908','Mais de 1908 a 2862','Mais de 2862 a 5724','Mais de 5724 a 9540', 'Mais de 9540 a 14310','Mais de 14310 a 23850','Mais de 23850']
-    const quantidadeDeUsuarios = ['17000','13000','20940','10000','4800','3400','1900',]
+    const nomeDasRedes = ['0,535','0,532','0,528','0,521','0,513','0,509','0,501','0,499','0,497','0.494']
+    const quantidadeDeUsuarios = ['2004','2005','2006','2007','2008','2009','2011','2012','2013','2014']
 
     const data = [
         {
@@ -22,7 +22,7 @@ async function quantidadeUsuariosPorRede() {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
         title: {
-            text: 'Distribuição das famílias brasileiras por renda',
+            text: 'Indice de GINI',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
@@ -33,7 +33,7 @@ async function quantidadeUsuariosPorRede() {
         xaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'Renda mensal familiar em R$',
+                text: 'Anos',
                 font: {
                     color: getCSS('--secondary-color')
                 }
@@ -42,7 +42,7 @@ async function quantidadeUsuariosPorRede() {
         yaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'número de famílias',
+                text: 'Rendimento Domiciliar',
                 font: {
                     color: getCSS('--secondary-color')
                 }
